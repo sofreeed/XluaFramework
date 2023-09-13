a04a721c045917020824ffb954e2e78a9bd2f90e
huzhibo <huzhibo@zeroogravity.com>
2023-09-13 16:56:03 +0800

    lua测试代码

--- Client/Assets/LuaScripts/UnitTest/CommonTest.lua
+++ Client/Assets/LuaScripts/UnitTest/CommonTest.lua
@@ -44,7 +44,38 @@ local function OriginalPackTest(...)
     local a1, b1, c1, d1, e1 = table.unpack(tab, 1, tab.n);
 end
 
+local function StringAndByte()
+    coroutine.waitforframes(1);
+    CS.Logger.LogError("11111111111111111111111111");
+
+    --例子1
+    local str = '258'
+    local ascii_str = string.pack('<H', str)
+    --H占2个字节 并且以小端存储，故存储的格式应该是下面的样子
+    -- 00000010 00000001
+    print(ascii_str)
+
+    --string.byte() 将一个ASCII字符还原回常用字符
+    local byte1 = ascii_str:byte(1)
+    --数据的低位，  输出 byte1 = 	2
+    local byte2 = ascii_str:byte(2)
+    --数据的高位 输出 byte2 = 	1
+
+    print('byte1 = ',byte1)
+    print('byte2 = ',byte2)
+
+    --所以，需要将 258 还原，只需要高位乘上 256 即可，即
+    local real_value = byte1 + byte2*256
+    --Logger.LogError(real_value)
+    --输出 258
+
+    CS.Logger.LogError("22222222222222222222222222");
+end
+
 local function Run()
+    --测试string和byte
+    StringAndByte();
+    
     --测试可变参数
     VariableParamTest(nil,1,nil,3,nil)      
     
