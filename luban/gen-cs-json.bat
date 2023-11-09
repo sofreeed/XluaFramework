ebce5da32d4a0ca7a37ae522e30271b07e65d3fb
huzhibo <huzhibo@zeroogravity.com>
2023-11-09 13:16:09 +0800

    no message

---
+++ luban/gen-cs-json.bat
@@ -0,0 +1,16 @@
+set WORKSPACE=.\
+
+set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set CONF_ROOT=%WORKSPACE%\MiniTemplate
+
+dotnet %LUBAN_DLL% ^
+    -t all ^
+    -c cs-simple-json ^
+    -d json  ^
+    --conf %CONF_ROOT%\luban.conf ^
+    -x outputCodeDir=..\Client\Assets\LuaScripts\Luban\Gen ^
+    -x outputDataDir=..\Client\Assets\LuaScripts\Luban\bytes ^
+    -x pathValidator.rootDir=%WORKSPACE%\Projects\Csharp_Unity_bin ^
+    -x l10n.textProviderFile=*@%CONF_ROOT%\l10n-texts.json
+
+pause
--- luban/gen-l10n.bat
+++ luban/gen-l10n.bat
@@ -7,7 +7,8 @@ dotnet %LUBAN_DLL% ^
     -t all ^
     -d text-list  ^
     --conf %CONF_ROOT%\luban.conf ^
-    -x outputDataDir=%CONF_ROOT%\out ^
+    -x outputDataDir=%CONF_ROOT%\l10nOutput ^
+	-x l10n.textListFile=keylist.txt ^
     -x l10n.textProviderFile=*@%CONF_ROOT%\l10n-texts.json
 
 pause
