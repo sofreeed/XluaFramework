b1b2c5545d9b7382d95934073bd13cd042826767
huzhibo <huzhibo@zeroogravity.com>
2023-11-09 10:38:13 +0800

    no message

--- luban/gen-lua.bat
+++ luban/gen-lua.bat
@@ -1,4 +1,4 @@
-set WORKSPACE=..\luban
+set WORKSPACE=.\
 
 set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
 set CONF_ROOT=%WORKSPACE%\MiniTemplate
@@ -8,9 +8,9 @@ dotnet %LUBAN_DLL% ^
     -c lua-lua ^
     -d lua  ^
     --conf %CONF_ROOT%\luban.conf ^
-    -x outputCodeDir=Assets\LuaScripts\Luban\Gen ^
-    -x outputDataDir=Assets\LuaScripts\Luban\bytes ^
+    -x outputCodeDir=..\Client\Assets\LuaScripts\Luban\Gen ^
+    -x outputDataDir=..\Client\Assets\LuaScripts\Luban\bytes ^
     -x pathValidator.rootDir=%WORKSPACE%\Projects\Csharp_Unity_bin ^
-    -x l10n.textProviderFile=*@%WORKSPACE%\DataTables\Datas\l10n\texts.json
+    -x l10n.textProviderFile=*@%CONF_ROOT%\l10n-texts.json
 
 pause
