9258833e3fe062d9e6008f550e1df547dc631b42
huzhibo <huzhibo@zeroogravity.com>
2023-12-28 10:28:29 +0800

    删除csharp框架代码

--- Client/Assets/Scripts/CSharp/Framework/UI/UIWindowNames.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UIWindowNames
-{
-    public const string Login = "Login";
-    public const string Hero = "Hero";
-}
